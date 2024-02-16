ab77dd74fdf0588c0042d1eb26c5b679996268b3
logonrmlocal <clara00017@hotmail.com>
2024-02-16 11:49:37 -0300

    aula 16/02

---
+++ 2° Semestre/src/index.ts
@@ -0,0 +1,17 @@
+var nome : string = 'Juscelino';
+var idade: number = 37;
+
+var alive : boolean = true;
+
+var qualquerCoisa : any = 'jfsofhs';
+
+var dataNasc : string | number = '02/1987';
+
+console.log(nome, idade, alive, qualquerCoisa, dataNasc);
+
+
+ const fun = (num1 : number, num2 : number) : number => {
+      return num1 + num2;
+ }
+
+ console.log(fun(5,7));
